5000c4c9f12757cc72290275fb48d0998d0950f7
unknown <jsyi1995@gmail.com>
2017-08-19 19:44:30 -0700

    1st official commit

--- client/src/navmenu.js
+++ client/src/navmenu.js
@@ -3,33 +3,38 @@ import { Link } from 'react-router-dom';
 
 class Navmenu extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {active: false};
+  constructor() {
+    super();
+    this.state = {
+        navmenuOpen: false
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
-
-  toggleMobile() {
-    this.setState({active: true});
+    handleClick() {
+    this.setState({
+      navmenuOpen: !this.state.navmenuOpen,
+    });
   }
 
   render() {
+    let openstate = this.state.navmenuOpen ? "nav-right nav-menu is-active" : "nav-right nav-menu"
     return (
       <div className="container">
         <div className="nav-left">
           <Link to='/' className="nav-item">OtakuQuotes</Link>
         </div>
-        <span className="nav-toggle" onClick={this.toggleMobile.bind(this)}>
+        <span className="nav-toggle" onClick={this.handleClick}>
           <span></span>
           <span></span>
           <span></span>
         </span>
-        <div className={"nav-right nav-menu " + (this.props.active ? 'is-active' : '')}>
+        <div className={openstate}>
           <Link to='/pending' className="nav-item">Pending</Link>
           <Link to='/search' className="nav-item">Search</Link>
           <Link to='/docs' className="nav-item">Docs</Link>
           <Link to='/submit' className="nav-item">Submit</Link>
           <span className="nav-item">
-            <a className="button is-success is-inverted">
+            <a className="button is-success is-inverted" href="https://github.com/xNinjaKittyx/OtakuQuotes">
               <span className="icon">
                 <i className="fa fa-github"></i>
               </span>
