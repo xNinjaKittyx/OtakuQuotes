22db660b8c90b7c6b4a372fb896baa5bbd2ec787
unknown <jsyi1995@gmail.com>
2017-08-30 23:59:21 -0700

    Major update #1

--- client/src/App.js
+++ client/src/App.js
@@ -8,40 +8,6 @@ import Random from './random.js';
 import Submit from './submit.js';
 import Navmenu from './navmenu.js';
 
-class FadeIn extends Component {
-
-  componentDidMount() {
-    console.log("componentDidMount", this.displayName);
-    var that = this;
-    // Get the components DOM node
-    var elem = ReactDOM.findDOMNode(that);
-    // Set the opacity of the element to 0
-    elem.style.opacity = 0;
-    window.requestAnimationFrame(function () {
-      // Now set a transition on the opacity
-      elem.style.transition = that.props.transition || "opacity 5000ms";
-      // and set the opacity to 1
-      elem.style.opacity = 1;
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-const MatchWithFade = ({ component: Component, transition, ...rest }) => (
-  <Route {...rest} render={(matchProps) => (
-    <FadeIn transition={transition}>
-      <Component {...matchProps} />
-    </FadeIn>
-  )} />
-)
-
 class App extends Component {
 
   constructor(props) {
@@ -56,12 +22,12 @@ class App extends Component {
         <div className="App">
           <Navmenu />
           <Switch>
-            <MatchWithFade exact path='/' component={Home} transition="opacity 3000ms"/>
-            <MatchWithFade path='/search' component={Search} transition="opacity 3000ms"/>
-            <MatchWithFade path='/about' component={About} transition="opacity 3000ms"/>
-            <MatchWithFade path='/pending' component={Pending} transition="opacity 3000ms"/>
-            <MatchWithFade path='/docs' component={Docs} transition="opacity 3000ms"/>
-            <MatchWithFade path='/submit' component={Submit} transition="opacity 3000ms"/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/search' component={Search}/>
+            <Route path='/about' component={About}/>
+            <Route path='/pending' component={Pending}/>
+            <Route path='/docs' component={Docs}/>
+            <Route path='/submit' component={Submit}/>
             <Route component={NoMatch} />
           </Switch>
         </div>
--- client/src/navmenu.js
+++ client/src/navmenu.js
@@ -1,40 +1,52 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 class Navmenu extends Component {
 
   constructor() {
     super();
     this.state = {
-        navmenuOpen: false
+        navmenuOpen: false,
+        value:''
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
     handleClick() {
     this.setState({
       navmenuOpen: !this.state.navmenuOpen,
     });
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
   render() {
     let openstate = this.state.navmenuOpen ? "navbar-menu is-active" : "navbar-menu"
+
     return (
-      <div className="container">
+      <div>
       <nav className="navbar navigation">
+        <div className="container">
         <div className="navbar-brand">
           <Link to='/' className="navbar-item white">OtakuQuotes</Link>
-          <div className="field has-addons controltop">
-              <p className="control is-expanded">
-                  <input className="input" type="text" placeholder="Search" />
-              </p>
-              <p className="control">
-                  <Link to='/search' className="button is-primary">
-                      <span className="icon is-small is-left">
-                          <i className="fa fa-search"></i>
-                      </span>
+            <div className="field has-addons controltop">
+              <div className="control">
+                <input className="input" placeholder="Search" value={this.state.value} onChange={this.handleChange}/>
+              </div>
+              <div className="control">
+                <Link to={{
+            pathname: '/search',
+            search: '?tags=' + this.state.value
+          }} className="button is-primary" onClick={this.forceUpdate}>
+                  <span className="icon is-small is-left">
+                      <i className="fa fa-search"></i>
+                  </span>
                   </Link>
-              </p>
-          </div>
+              </div>
+            </div>
           <div className="navbar-burger white" onClick={this.handleClick}>
             <span></span>
             <span></span>
@@ -45,7 +57,6 @@ class Navmenu extends Component {
           <div className="navbar-end">
             <Link to='/about' className="navbar-item white">About</Link>
             <Link to='/pending' className="navbar-item white">Pending</Link>
-            <Link to='/search' className="navbar-item white">Search</Link>
             <Link to='/docs' className="navbar-item white">Docs</Link>
             <Link to='/submit' className="navbar-item white">Submit</Link>
             <span className="navbar-item">
@@ -57,6 +68,7 @@ class Navmenu extends Component {
               </a>
             </span>
           </div>
+        </div>
         </div>
       </nav>
       </div>
